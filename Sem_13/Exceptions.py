a7a9ac9ee1daaf4f404e8e8e366c543f70aa5dca
kushnir-n <kushn1r-natalya@yandex.ru>
2023-06-28 09:54:55 +0300

    hw

---
+++ Sem_13/Exceptions.py
@@ -0,0 +1,30 @@
+
+class UserException(Exception):
+    pass
+
+
+class UserNumberError(UserException):
+    
+    def __init__(self, value):
+        self.value = value
+    
+    def __str__(self):
+        return f'Нельзя ввести отрицательное число или число больше 100 тыс'
+    
+
+class UserTriangleError(UserException):
+    
+    def __init__(self, value):
+        self.value = value
+    
+    def __str__(self):
+        return f'Нельзя создать треугольник, в котором одна сторона больше суммы двух других. Треугольника с такими сторонами не существует'
+    
+
+class UserDigitError(UserException):
+    
+    def __init__(self, value):
+        self.value = value
+    
+    def __str__(self):
+        return f'Нужно ввести целое число!'
